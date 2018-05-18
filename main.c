47126c511967e20e8bd8858ed046a232564d55fe
alielmartins <alielmartins@icloud.com>
2018-05-17 23:22:18 -0300

    Versão: 1.0

--- main.c
+++ main.c
@@ -1,152 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h> // socket(); 
-#include <sys/types.h>
-#include <netinet/in.h> // IPPROTO_TCP
-#include <unistd.h> // close();
-#include <string.h> // memset();
-#include <arpa/inet.h> // inet_ntoa();
-#include <pthread.h> // thread();
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
 
-#define porta_servidor 80
-#define qnt_max 255
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int Start_Servidor(int port);
-int black_list(struct sockaddr_in list);
-void * receber_cliente(char * params);
-char * filtro_pacotes(char * buffer,size_t tamanho);
-char * montar_pacote(char * pagina);
-char * proxy(char * pacote,size_t tamanho);
-
-void logs(char * texto);
-
-struct infos_cli{
-	int socket;
-	struct sockaddr_in settings;
+struct clients_infos{									// Struct necessaria para passar informações para thread
+    int socket_id;
+    struct sockaddr settings;
 };
 
-void main(int args,char * params[]){
-	printf("[!] Inicializando o servidor \n");
-	Start_Servidor(porta_servidor); // Inicializa o servidor na porta 80
+void log(char * txt);									// Função que escreve no arquivo de LOG
+int Start_Server(int port);								// Função que inicializa o servidor na porta passada como (int)parametro
+void * clients(void * params);								// Função que trata o cliente
+
+int main(int, char * params[]){
+    Start_Server(80);                                                                   // Inicialização de servidor na porta 80
 }
 
-int Start_Servidor(int port){
-	pthread_t threads=0;
-	int servidor_pointer=0,conexao=0,clients=0,clients_lenght=0;
-	struct sockaddr_in settings,clients_settings;
-	struct infos_cli cliente_infos;
-	// Definições de propriedades de servidor
-	servidor_pointer = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-	memset(&settings,0,sizeof(settings)); // String!
-	settings.sin_family = AF_INET;
-	settings.sin_addr.s_addr = INADDR_ANY;
-	settings.sin_port = htons(port);
-	// --------------------------------------
+int Start_Server(int port){
+    int Socket = 0,connection=0,clients_socket=0;
+    pthread_t threads=0;
+    socklen_t clients_size=0;
+    struct sockaddr_in Settings;                                                        // Struct com configurações do servidor
+    struct sockaddr_in clients_settings;                                                // Struct para receber informações do cliente
+    struct clients_infos infos_clients;                                                 // Struct para passar informações do cliente
+    memset(&Settings,0,sizeof(Settings));                                               // Limpa a struct com configurações do servidor
+    memset(&clients_settings,0,sizeof(clients_settings));                               // Limpa a struct com configurações do cliente
+    memset(&infos_clients,0,sizeof(infos_clients));                                     // Limpa a struct com informações do cliente
+    Socket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);                                   // Define regras da conexão
+    if(Socket == -1){                                                                   // Verifica se o socket foi definido com sucesso
+        printf("[F] Falha em definir as configurações de  socket. \n");
+        return 0;
+    }
+    log((char*)"[SOCKET] Socket definido com sucesso. \n");                             // Adiciona no arquivo de log
+    Settings.sin_family = AF_INET;                                                      // Define a familia do servidor
+    Settings.sin_addr.s_addr = INADDR_ANY;                                              // Inicializa o servidor no IP principal da maquina
+    Settings.sin_port = htons(80);                                                      // Define a porta a ser utilizada
+    if( (connection = bind(Socket,(struct sockaddr*)&Settings,sizeof(Settings)))==-1){  // Inicializa o bind e verifica se foi realizado com sucesso
+        printf("[F] Falha em inicializar o bind. \n");
+        return 0;
+    }
+    log((char*)"[SOCKET] Bind inicializado com sucesso \n");                            // Adiciona no arquivo de log
+    listen(Socket,255);                                                                 // Quantidade de conexões suportadas pelo Socket
+    printf("[Ok] Aguardando Conexão \n");
+    clients_size = sizeof(struct sockaddr_in);
+    while(1){
+        clients_socket = accept(Socket,(struct sockaddr*)&clients_settings,&clients_size);      // Estabelece conexão com cliente
+        if(clients_socket == -1){
+            printf("[F] Falha em estabelecer conexão com cliente \n");
+        }
+        printf("[Ok] Cliente %s:%d conectado com o servidor. \n",inet_ntoa(clients_settings.sin_addr),port);
+        infos_clients.socket_id = clients_socket;                                       // Passa valor do socket do cliente
+        memcpy(&infos_clients.settings,&clients_settings,sizeof(clients_settings));     // Passa configurações  (IP,PORTA,FAMILIA)
+        pthread_create(&threads,NULL,&clients,&infos_clients);
+        pthread_join(threads,NULL);
+    }
+    close(Socket);                                                                      // fecha o socket
 
-	if(servidor_pointer == 0){
-		printf("[F] Falha em inicializar o socket. \n");
-		return 0;
-	}else{
-		printf("[OK] Servidor inicializado com sucesso. \n");
-	}
-	conexao = bind(servidor_pointer,(struct sockaddr*)&settings,sizeof(struct sockaddr));
-	if(conexao != 0){
-		printf("[F] Falha no bind. \n");
-		return 0;
-	}else{
-		printf("[Ok] Bind inicializado com sucesso. \n");		
-	}
-	listen(servidor_pointer,qnt_max);
-	clients_lenght = sizeof(struct sockaddr_in);
-	printf("[!] Aguardando clientes [!] \n");
-	while(1){
-		clients = accept(servidor_pointer,(struct sockaddr*)&clients_settings,&clients_lenght);
-		if(clients == -1){
-			printf("[F] erro em aceitar cliente! \n");
-		}else{
-			printf("	[Ok] Cliente %s:%d conectado \n",inet_ntoa(clients_settings.sin_addr),porta_servidor);
-			cliente_infos.socket = clients;
-			memcpy(&cliente_infos.settings,&clients_settings,sizeof(struct sockaddr));
-			if(black_list(clients_settings) != 0){
-				pthread_create(&threads,NULL,(void*)receber_cliente,(void*)&cliente_infos);
-				pthread_join(threads,NULL);
-			}else{
-				printf("	[Ok] Cliente %s:%d banido \n",inet_ntoa(clients_settings.sin_addr),porta_servidor);
-				close(clients);	
-			}
-			
-		}
-	}
-	close(servidor_pointer);
 }
-void * receber_cliente(char * params){ 
-	char * buffer;
-	size_t tamanho = 1000000,nbytes=0;
-	buffer = malloc(tamanho);
-	struct infos_cli cliente;
-	memcpy(&cliente,params,sizeof(struct infos_cli));
-	while(1){
-		if( (nbytes = recv(cliente.socket,buffer,tamanho,0)) == 0){
-			printf("	[Ok] Cliente %s:%d desconectado \n",inet_ntoa(cliente.settings.sin_addr),porta_servidor);
-			free(buffer);
-			break;
-		}else{
-			buffer = filtro_pacotes(buffer,nbytes); 
-			if(buffer == NULL){
-				free(buffer);
-				close(cliente.socket);
-				return NULL;
-			}
-			if(send(cliente.socket,buffer,tamanho,0) == 0){
-				printf("[Ok] Cliente com resposta \n"); 
-				free(buffer);
-				break;
-			}
-			memset(buffer,0,tamanho);
-		}
-	}
-	close(cliente.socket);
-	return NULL;
+void log(char * txt){
+    FILE * wLogs;                                                                       // Aponta para endereço do arquivo
+    wLogs = fopen("logs.txt","a");                                                      // Abre o arquivo logs.txt caso não exista seja criado
+    if(wLogs == NULL){                                                                  // Verifica se criou ou abrio com sucesso
+        printf("[F] Falha em abrir o arquivo de log. \n");
+        return;
+    }
+    fprintf(wLogs,"%s",txt);                                                            // escreve os dados no arquivo
 }
-void logs(char * texto){
-	FILE * wlogs;
-	wlogs = fopen("logs.txt","a+");
-	if(wlogs == NULL){
-		printf("[F] Falha em escrever no arquivo de log \n");
-		return;
-	}else{
-		fprintf(wlogs,"%s",texto);
-	}
-	fclose(wlogs);
+void * clients(void * params){                                                          // Função que executa funções para o cliente
+    struct clients_infos infos_clientes;
+    memcpy(&infos_clientes,params,sizeof(infos_clientes));                              // Recupera os dados do cliente
+
+    // CODIGO PARA INTERPRETAR OS PACOTES TCP/TCP RECEBIDOS PELO SERVIDOR
+
+    // ------------------------------------------------------------------
+    close(infos_clientes.socket_id);                                                    // Finaliza conexão com o cliente
+    return NULL;
 }
 
-int black_list(struct sockaddr_in list){
-	// Função retorna 1 caso permitido e 0 caso o IP esteja banido
-	// Black_List do servidor
-
-	printf("	Verificando IP: %s \n",inet_ntoa(list.sin_addr));
-	return 1;
-}
-char * filtro_pacotes(char * buffer,size_t tamanho){
-	// Verifica que tipo de pedido é, caso seja aceitavel pelo servidor retorna um ponteiro para resposta!
-	printf("Conteudo: %s \n",buffer);
-	/* char * paginas = NULL;
-	paginas = strtok(buffer,"\n");
-	paginas = strtok(&paginas[4]," ");
-	char * resposta;
-	if(1){ // SE FOR HTTP
-		resposta = proxy(montar_pacote(paginas),tamanho);
-		if(resposta == NULL){
-			printf("[F] Falha na resposta do proxy. \n");
-			return NULL; // Retornar pagina de ERROR INTERNO NO PROXY
-		}else{
-			printf("[Ok] Pagina requisitada devoldida para o cliente. \n");
-			return resposta;
-		}
-	}else{
-		printf("[F] Falha no tratamento do pacote. \n");
-		return NULL;     // Retornar pagina de ERROR BAD REQUEST
-	}
-	*/
-	return NULL;
-}
